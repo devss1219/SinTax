8de894ed5725cb1ffc21916ae61d64dc1be0932b
devss1219 <ershobhit07@gmail.com>
2026-06-28 12:45:46 +0530

    Frontend: Configure manifest and vite for Chrome Extension

--- frontend-ext/vite.config.js
+++ frontend-ext/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // Yeh import add karo
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()], // Yahan tailwindcss() add karo
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist', // Extension ki output files yahan jayengi
+  }
 })
